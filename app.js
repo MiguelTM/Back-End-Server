fac83f95fa61291b64043288046ffcdf17281fc7
MAT14 <migueltrabajos22@gmail.com>
2018-07-22 23:30:41 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 
 
 
 // Inicializar variables
 var app = express();
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+//importar rurtas 
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 //conexion to db
 
@@ -16,25 +27,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
     console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
 
-
 });
 
 
 // Rutas
 
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-
-        ok: true,
-        mensaje: 'Peticion correcta'
-    });
-
-});
-
-
-
-
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 
